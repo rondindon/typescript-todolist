d40a431fa886e083dcb148b4e6098216e3ca9f38
rondindon <ronnie.rybarik@gmail.com>
2023-08-22 01:07:21 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      22.08.2023
+
+    </div>
+  );
+}
+
+export default App;
