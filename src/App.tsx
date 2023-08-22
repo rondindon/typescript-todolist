5bccf57e356869d98dff9bb88fd5672fda328d1f
rondindon <ronnie.rybarik@gmail.com>
2023-08-22 19:37:12 +0200

    typescript basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let name: string;
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role:[number,string];
+let anyType :any;
+let unknownType : unknown; // recommended instead of any
+
+let printName: (name:string) => never; //void returns undefied and never doesnt return anything
+
+// function printName(name:string){
+//   console.log(name)
+// }
+
+// printName("ronko")
+
+// type Person = {
+//   name: string;
+//   age? : number;
+// }
+
+// let person:Person= {
+//   name : "ROnko",
+// };
+
+// let lotsOfPeople: Person[];
+
+interface Person {
+  name : string;
+  age? : number;
+}
+
+interface Guy extends Person {
+  proffesion : string;
+}
 
 function App() {
   return (
