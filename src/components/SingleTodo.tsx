5277c03e7c90cca8dd6ed03f11432b78d9124152
rondindon <ronnie.rybarik@gmail.com>
2023-08-23 17:54:22 +0200

    delete and done function

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Todo } from './Model';
+import {AiFillEdit,AiFillDelete} from 'react-icons/ai'
+import {MdDone} from 'react-icons/md'
+import { Todolist } from './Todolist';
 
 interface Props {
     todo: Todo;
@@ -8,8 +11,36 @@ interface Props {
 }
 
 export const SingleTodo = ({todo,todos,setTodos} : Props) => {
+
+    const handleDone = (id: number) =>{
+        setTodos(todos.map(todo => todo.id === id ? {...todo,isDone:!todo.isDone} : {...todo}))
+    }
+
+    const handleDelete = (id: number) =>{
+        setTodos(todos.filter((todo) => todo.id !== id))
+    }
+
   return (
-    <div>SingleTodo</div>
+    <form className='todos-single'>
+        {
+            todo.isDone ? (
+
+                <s className='todos-single-text'>{todo.todo}</s>
+            ) 
+            : (
+                <span className='todos-single-text'>{todo.todo}</span>
+            )
+        }
+
+        <div className='icons'>
+
+            <span className='icon'><AiFillEdit /></span>
+            <span className='icon' onClick={() => {handleDelete(todo.id)}}><AiFillDelete /></span>
+            <span className='icon' onClick={() => {handleDone(todo.id)}}><MdDone /></span>
+
+        </div>
+
+    </form>
 
     )
 }
